5d6fd6bf84a7bad23782b6412fa2e944c9e8c1e1
choiOV <121269364+choiOV@users.noreply.github.com>
2025-06-20 19:00:48 +0900

    Solve: compressWord

--- compressWord.js
+++ compressWord.js
@@ -1,5 +1,6 @@
 // K개의 연속된 같은 문자가 있으면, 그 문자들을 제거한다. (더 이상 제거할 수 없을 때까지 반복한다.)
 
+// 첫 번째 풀이
 function compressWord(word, K) {
   if (K === 0) return word;
 
@@ -24,4 +25,23 @@ function compressWord(word, K) {
   }
 
   return arrWord.join("");
+}
+
+// 두 번째 풀이
+function compressWord(word, K) {
+  const stack = [];
+
+  for (const str of word) {
+    stack.push(str);
+
+    if (stack.length >= K) {
+      const checkSameCharArr = stack.slice(-K);
+
+      if (checkSameCharArr.every((e, i, a) => a[0] === e)) {
+        stack.splice(-K);
+      }
+    }
+  }
+
+  return stack.join("");
 }
