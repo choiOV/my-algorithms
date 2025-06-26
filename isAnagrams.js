8f2d1d9d2df8eaa4d268a971ce0a6eaa82a95716
choiOV <121269364+choiOV@users.noreply.github.com>
2025-06-26 16:59:33 +0900

    Solve: isAnagrams

--- isAnagrams.js
+++ isAnagrams.js
@@ -36,3 +36,27 @@ function isAnagrams(s, t) {
 
   return hashArray.every((count) => count <= 0);
 }
+
+// 두 번째 풀이
+function isAnagrams(s, t) {
+  if (s.length !== t.length) return false;
+
+  const normalize = (str) =>
+    [...str.toLowerCase()].filter((c) => c >= "a" && c <= "z");
+
+  const sArr = normalize(s);
+  const tArr = normalize(t);
+  const counter = {};
+
+  for (const c of sArr) {
+    counter[c] = (counter[c] || 0) + 1;
+  }
+
+  for (const c of tArr) {
+    if (!counter[c]) return false;
+
+    counter[c] -= 1;
+  }
+
+  return true;
+}
